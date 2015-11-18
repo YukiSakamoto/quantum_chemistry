53c1a8dfb4405a6682dc7684ed256dad9183eeba
坂本裕紀 <sakamoto@nakata-hiroya-no-iMac.local>
2015-11-18 18:05:00 +0900

    add slater type function

--- hf.py
+++ hf.py
@@ -2,22 +2,34 @@ import numpy as np
 import math
 # Hartree Fock Method
 
-Debug_enable = False
+Debug_enable = True
 
-def debug_output(text):
+def debug_output(value):
     if Debug_enable:
-        print text
+        print value
+    return value
 
 # SC = FCe
 
-nuclears = [
-        ((0.0, 0.0, 0.0), 1.0 ), 
-        ((1.0, 0.0, 0.0), 2.0 ),
-        ]
 
 atomic_orbitals = [
     # (Function, center_position)
 ]
+
+def norm(start, end):
+    dX = end[0] - start[0]
+    dY = end[1] - start[1]
+    dZ = end[2] - start[2]
+    return math.sqrt( dX ** 2 + dY ** 2 + dZ ** 2 )
+
+class SlaterTypeFunction:
+    def __init__(self, center, exponent, coeff = 1):
+        self.center = center
+        self.exponent = exponent
+        self.coeff = coeff
+    def value(self, pos):
+        r = norm(pos, self.center)
+        return self.coeff * math.exp(self.exponent * r)
 
 def gaussian_function_product(orbit_exponent1, centre1, orbit_exponent2, centre2):
     # Just store short named variables.
@@ -32,6 +44,7 @@ def gaussian_function_product(orbit_exponent1, centre1, orbit_exponent2, centre2
     return (r, Rc, k)
 
 def overlap_integral():
+    # S_ij = <phi_i | phi_j>
     return None
     
 def double_integral():
@@ -72,13 +85,18 @@ def scf():
 a = np.matrix([[1,2,3], [4,5,6], [7,8,9]])
 b = np.matrix([[1,2,3], [4,5,6], [7,8,9]])
 
-print a
-print b
+#print a.dot(b)
 
-print a.dot(b)
+#c = np.matrix([[2,1],[1,1]])
 
-c = np.matrix([[2,1],[1,1]])
+#c = np.matrix([[1,0,0], [0,1,0], [0,0,1]])
+#print np.linalg.inv(c)
 
-c = np.matrix([[1,0,0], [0,1,0], [0,0,1]])
-print np.linalg.inv(c)
+nuclears = [
+        ((0.0, 0.0, 0.0), 1.0 ), 
+        ((1.0, 0.0, 0.0), 2.0 ),
+        ]
+
+f = SlaterTypeFunction( (0.0, 0.0, 0.0), 2.0, 1.0 )
+print f.value((1.0, 0.0, 0.0) )
 
